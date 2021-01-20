6b34f919d59c41b9431faa9640532e604c065964
jhayX <lordsylharx@gmail.com>
2021-01-21 00:14:56 +0100

    fixed minor bugs

--- src/layouts/Admin.vue
+++ src/layouts/Admin.vue
@@ -1,7 +1,7 @@
 <template>
   <div>
     <sidebar />
-    <div class="relative md:ml-64 bg-white">
+    <div class="relative md:ml-56 bg-white">
       <header-stats />
       <div class="px-4 md:px-10 mx-auto w-full -m-24">
         <router-view />

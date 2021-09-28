f461f8ece3fdf306c3b7396f1331bb1aee8f7678
cheedinmar <cheedinmarblessing@gmail.com>
2021-09-28 18:23:42 +0100

    formatting code

--- src/components/buttons/buttons.vue
+++ src/components/buttons/buttons.vue
@@ -1,58 +1,51 @@
 <template>
-    <div>
-            <router-link
-                :to="{ name: 'Result', params: { name: rpayDuration.name } }"
-                
-                >
-            <button
-                class="
-                w-full
-                focus:outline-none
-                font-semibold
-                lg:text-lg
-                text-base
-                rounded-lg
-                px-5
-                py-3
-                bg-white
-                group-focus:text-yellow-300
-                "
-                :class="styleActiveRoute"
-                                @click="getResultMobile(2, 20)"
-            >
-
-                {{ rpayDuration.name }}
-            </button>
-                </router-link>
-        </div>                  
-    
+  <div>
+    <router-link :to="{ name: 'Result', params: { name: rpayDuration.name } }">
+      <button
+        class="
+          w-full
+          focus:outline-none
+          font-semibold
+          lg:text-lg
+          text-base
+          rounded-lg
+          px-5
+          py-3
+          bg-white
+          group-focus:text-yellow-300
+        "
+        :class="styleActiveRoute"
+        @click="getResultMobile(2, 20)"
+      >
+        {{ rpayDuration.name }}
+      </button>
+    </router-link>
+  </div>
 </template>
 
 <script>
 export default {
-    props:{
-        
-        getResultMobile:{
-            type: Function,
-            required: true
-        },
-        rpayDuration:{
-            type:Object
-        }                 
+  props: {
+    getResultMobile: {
+      type: Function,
+      required: true,
     },
-    computed:{
-    styleActiveRoute(){
-      let color = '';
-      if(this.$route.path == '/calculator/'+ this.rpayDuration.name ){
-        return color = 'bg-red-700 text-white '
+    rpayDuration: {
+      type: Object,
+    },
+  },
+  computed: {
+    styleActiveRoute() {
+      let color = "";
+      if (this.$route.params.name == this.rpayDuration.name) {
+        return color = "bg-red-700 text-white ";
+      } else {
+        return color = "";
       }
-    } 
-    }
-    
-
-}
+    },
+  },
+};
 </script>
 
 <style>
-
 </style>

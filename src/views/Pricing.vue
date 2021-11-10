3f5809f75bc40cce6afbb5381cfc60366014c10c
cheedinmar <cheedinmarblessing@gmail.com>
2021-11-10 20:35:44 +0100

    back button added

--- src/views/Pricing.vue
+++ src/views/Pricing.vue
@@ -2,15 +2,38 @@
   <div class="bg-white">
     <index-navbar />
     <div
-      class="w-full h-full flex flex-col items-center bg-white bg-no-repeat"
+      class="
+        w-full
+        h-full
+        flex flex-col
+        items-center
+        bg-white bg-no-repeat
+      "
       :style="`background-image: url('${registerBg2}');`"
     >
+    <div class="w-full  mt-20 flex justify-end">
+                 <router-link to="/" class=" mr-12">
+              <button
+                class="
+                  altaraBlue
+                  text-white
+                  rounded
+                  px-8
+                  py-2
+                  cursor-pointer
+                  font-semibold
+                  text-base
+                "
+              >
+                Back
+              </button>
+            </router-link>
+    </div>
+      
       <div
         class="
           w-full
-          mt-10
           lg:mb-10
-          md:h-screen
           md:flex-row
           flex flex-col
           space-y-10
@@ -31,10 +54,10 @@
             duration-500
             ease-in-out
             transform
-            hover:-translate-y-1
-            hover:scale-110
+            hover:-translate-y-1 hover:scale-110
             flex
             items-center
+            my-12
             flex-col
             h-auto
             py-8
@@ -75,10 +98,10 @@
             duration-500
             ease-in-out
             transform
-            hover:-translate-y-1
-            hover:scale-110
+            hover:-translate-y-1 hover:scale-110
             flex
             items-center
+            my-12
             flex-col
             h-auto
             py-8
